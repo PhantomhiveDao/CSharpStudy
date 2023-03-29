4d70c6a2d2fd03b4679c7255f73415b6221dd9aa
DaoZhang <1076354486@qq.com>
2023-03-29 22:46:14 +0800

    代码与笔记更新至‘线程池’

--- CSharp4_siki/unit21_线程池/Program.cs
+++ CSharp4_siki/unit21_线程池/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace unit21_线程池
 {
@@ -6,7 +7,20 @@ namespace unit21_线程池
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(DowloadA);
+                //用线程池启用的线程默认为后台线程
+            }
+            Thread.Sleep(5000);
+        }
+        static void DowloadA(Object state)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Dowloading..."+Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
